71acb0e7b1d4aa6c419428dde49700f326fd652f
diegourielvr <diegourielvr@gmail.com>
2026-05-17 14:42:44 -0600

    feat(nvim): add mini indentscope

---
+++ plugin/mini_indentscope.lua
@@ -0,0 +1,15 @@
+local add_on_event = require('vim-pack').add_on_event
+local vbar_icon = require('icons').misc.vertical_bar
+
+add_on_event('BufEnter', {
+    {
+        src = 'echasnovski/mini.indentscope',
+        version = 'stable',
+        opts = {
+            symbol = vbar_icon, -- "▏"
+            options = {
+                try_as_border = true
+            },
+        },
+    }
+})
