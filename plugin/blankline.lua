71dccd9be83d954288fa0d7e480e423cb7f160d5
diegourielvr <diegourielvr@gmail.com>
2026-05-17 14:51:46 -0600

    feat(nvim): add blankline

---
+++ plugin/blankline.lua
@@ -0,0 +1,15 @@
+local add = require('vim-pack').add
+local vbar_icon = require('icons').misc.vertical_bar
+
+add({
+    {
+        src = 'lukas-reineke/indent-blankline.nvim',
+        module_name = 'ibl',
+        opts = {
+            indent = {
+                char = '│',
+                tab_char = '│',
+            },
+        },
+    }
+})
